c43f7a6adaf26c6d820f711008083fcb83012b92
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-06-18 07:45:21 -0600

    - Añadiendo la clase vertex y una versión preliminar funcional del makefile

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include "pageRank.h"
+#include "vertex.h"
+
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        std::cerr << "Usage: " << argv[0] << " <inputFile> <outputFile> <iterations> <dampingFactor>" << std::endl;
-        return 1;
-    }
 
-    std::string inputFile = argv[1];
-    std::string outputFile = argv[2];
-    int iterations = std::stoi(argv[3]);
-    double dampingFactor = std::stod(argv[4]);
+    // argv[2] (Aqui esta el archivo a usar)
+    printf("Argumento 1 %s \n", argv[2]);
 
-    PageRank pageRank(inputFile, outputFile, iterations, dampingFactor);
-    pageRank.calculatePageRank();
+    // argv[4] (Aqui esta la ruta a guardar el archivo)
+    printf("Argumento 2 %s\n", argv[4]);
 
-    return 0;
+    // if (argc != 5) {
+    //     std::cerr << "Usage: " << argv[0] << " <inputFile> <outputFile> <iterations> <dampingFactor>" << std::endl;
+    //     return 1;
+    // }
+
+    // std::string inputFile = argv[1];
+    // std::string outputFile = argv[2];
+    // int iterations = std::stoi(argv[3]);
+    // double dampingFactor = std::stod(argv[4]);
+
+    // PageRank pageRank(inputFile, outputFile, iterations, dampingFactor);
+    // pageRank.calculatePageRank();
+
+    // return 0;
 }
---
+++ makefile
@@ -0,0 +1,28 @@
+# Verificar si ARGS está definido
+ifndef ARGS
+$(error No se proporcionaron las flags requeridas. Ejecute con make ARGS="-src "archivo a usar" -dst "ruta a guardar el resultado"")
+endif
+
+all: compile_general execute_general clean
+
+# Variables
+INPUTFILE = prueba.txt
+FILES = main.cpp
+EXECUTABLE = executeme
+
+# COMPILERS INSTANCES pthread
+compile_general:
+	g++ -o $(EXECUTABLE) $(FILES)
+
+compile_general_cuda:
+	nvcc add.cu -o add_cuda
+
+clean:
+	@echo "\n"
+	rm -f $(EXECUTABLE)
+
+# EXECUTERS INSTANCES
+execute_general:
+	@echo "\n"
+	./$(EXECUTABLE) $(ARGS)
+	@echo "\n"
