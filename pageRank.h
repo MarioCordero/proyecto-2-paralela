69dece78307cd38c285a133b1a29f105993dfe6e
Geiner <monba3122@gmail.com>
2024-06-26 23:06:46 -0600

    pageRank a openMP

--- pageRank.h
+++ pageRank.h
@@ -2,39 +2,67 @@
 #define PAGERANK_H
 
 #include "vertex.h"
-#include <string>
-#include <map>
+#include <iostream>
+#include <omp.h>
 #include <vector>
-#include <pthread.h>
-
-// Estructura para pasar los datos a los hilos
-struct ThreadData {
-    std::map<int, Vertex*>& verticesMap; // Referencia al mapa de vértices
-    pthread_mutex_t* mutex; // Mutex para sincronización
-};
+#include <unordered_map>
 
 class PageRank {
-private:
-    int iterations; // Número de iteraciones para el cálculo de PageRank
-    int numThreads; // Número de hilos a utilizar
-    pthread_mutex_t mutex; // Mutex para sincronización entre hilos
-    std::map<int, Vertex*> verticesMap; // Mapa de vértices
-
 public:
-    // Constructor
-    PageRank(int iterations, const std::string& inputFile, int numThreads);
-
-    // Destructor
+    PageRank(int iterations, int numThreads);
     ~PageRank();
 
-    // Función que será ejecutada por cada hilo para actualizar las puntuaciones de las páginas
-    static void* updateRanksThread(void* arg);
-
-    // Método para actualizar las puntuaciones de las páginas utilizando múltiples hilos
     void updateRanks();
-
-    // Método para calcular el PageRank
     void calculatePageRank();
+
+private:
+    int iterations;
+    int numThreads;
+    std::unordered_map<int, Vertex*> verticesMap;
 };
+
+// Constructor de la clase PageRank
+PageRank::PageRank(int iterations, int numThreads)
+    : iterations(iterations), numThreads(numThreads) {}
+
+// Destructor de la clase PageRank
+PageRank::~PageRank() {}
+
+// Método para actualizar las puntuaciones de las páginas utilizando múltiples hilos con OpenMP
+void PageRank::updateRanks() {
+    int verticesPerThread = (verticesMap.size() + numThreads - 1) / numThreads; // División redondeada hacia arriba
+
+    // Configurar el número de hilos en OpenMP
+    omp_set_num_threads(numThreads);
+
+    // Paralelizar el bucle usando OpenMP
+    #pragma omp parallel for
+    for (int i = 0; i < verticesMap.size(); ++i) {
+        auto it = std::next(verticesMap.begin(), i);
+        Vertex* vertex = it->second;
+
+        // Imprimir el vértice actual y la lista de vértices que le apuntan (sección crítica)
+        #pragma omp critical
+        {
+            std::cout << "Thread ID: " << omp_get_thread_num() << " - Vertex ID: " << vertex->getID() << std::endl;
+            const auto& pointingVertices = vertex->getPointingVertices();
+            std::cout << "Pointing vertices: ";
+            for (auto* pointingVertex : pointingVertices) {
+                std::cout << pointingVertex->getID() << " ";
+            }
+            std::cout << std::endl;
+        }
+
+
+    }
+}
+
+// Método para calcular el PageRank
+void PageRank::calculatePageRank() {
+
+    for (int i = 0; i < iterations; ++i) {
+        updateRanks();
+    }
+}
 
 #endif // PAGERANK_H
