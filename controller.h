f79f61b666ff34b63c4ad12953ccc6bc13b4aea8
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-06-29 19:10:30 -0600

    - Observaciones de clase controladora -[x]

--- controller.h
+++ controller.h
@@ -1,7 +1,6 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-// Bibliotecas e inclusion de encabezados
 #include "fileManager.h"
 #include <iostream>
 #include <string>
@@ -9,55 +8,60 @@
 
 using namespace std;
 
-class Controller
-{
-private:
-public:
-    int start(int argc, char *argv[]);
-}; // Fin clase Controller
+class Controller{
+    private:
+    public:
+        int start(int argc, char *argv[]);
+};
 
 // ---------------------------------------IMPLEMENTACIÓN---------------------------------------//
 
 // Inicializador de clase controladora
-int Controller::start(int argc, char *argv[])
-{
-    string fileName = "";
-    string fileDestination = "";
+int Controller::start(int argc, char *argv[]){
 
-    if (strcmp(argv[1], "-src") == 0 && strcmp(argv[3], "-dst") == 0)
-    {
-        // argv[2] (Aqui esta el archivo a usar)
-        fileName = argv[2];
-        // argv[4] (Aqui esta la ruta a guardar el archivo)
-        fileDestination = argv[4];
-    }
-    else
-    {
-        if (strcmp(argv[1], "-dst") == 0 && strcmp(argv[3], "-src") == 0)
-        {
-            // argv[4] (Aqui esta el archivo a usar)
-            fileName = argv[4];
-            // argv[2] (Aqui esta la ruta a guardar el archivo)
-            fileDestination = argv[4];
-        }
-        else
-        {
-            // Devolver codigo de error -1
+    std::string fileName = "";
+    std::string fileDestination = "";
+
+    // Iterar sobre los argumentos dados
+    int i = 1;
+    while (i < argc) {
+
+        if (strcmp(argv[i], "-src") == 0 && i + 1 < argc) {
+
+            fileName = argv[i + 1];
+            i += 2;
+
+        } else if (strcmp(argv[i], "-dst") == 0 && i + 1 < argc) {
+
+            fileDestination = argv[i + 1];
+            i += 2;
+
+        } else {
+            // Código de error -1
             return -1;
-        } // Fin if
-    } // Fin if
+        }
+    }
+
+    // Verificar que ambos parámetros se hayan especificado
+    if (fileName.empty() || fileDestination.empty()) {\
+        // Código de error -1
+        return -1;
+    }
 
     // Crear una instancia de la clase FileManager
     FileManager fileProcessor;
 
-    if (fileProcessor.processFile(fileName, fileDestination))
-    {
-    }
-    else
-    {
+    if (fileProcessor.processFile(fileName, fileDestination)){
+
+        // -TODO[] : HACER LO QUE QUEDA
+
+    }else{
+
         return -1;
-    } // Fin if
+
+    }
+
     return 0;
-} // Fin start
+}
 
 #endif // Fin CONTROLLER_H
