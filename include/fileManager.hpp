288a343adbada472a5509de454799503bf23f4d7
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-07-02 10:02:08 -0600

    - Cambio atributos de fileManager

--- include/fileManager.hpp
+++ include/fileManager.hpp
@@ -19,6 +19,8 @@ class FileManager
 private:
     // Mapa para almacenar los nodos y sus asociaciones
     unordered_map<int, vertex> nodeAssociations;
+    // Nombre del archivo para escribir
+    string outputFileName;
 
 public:
     // Constructor
@@ -34,7 +36,7 @@ public:
     void writeFile();
     // Establecer el nombre del archivo
     void setOutputFileName(const string &fileName);
-    
+
 }; // Fin clase FileManager
 
 #endif // Fin FILEMANAGER_H
